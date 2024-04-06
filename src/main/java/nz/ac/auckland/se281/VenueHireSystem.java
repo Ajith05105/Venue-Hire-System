bb47e667e4c1e2afa6d086273fcf2d20dd5c5be7
Ajith <ajith.penmatsa001@gmail.com>
2024-04-07 00:53:08 +1300

    ran all the hidden tests and made the necessary changes

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -5,12 +5,11 @@ import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
 
 public class VenueHireSystem {
-  private ArrayList<ArrayList<String>> venueData;
+  private ArrayList<venue> allTheVenues = new ArrayList<venue>();
   int bookings;
   private String dateInput;
 
   public VenueHireSystem() {
-    venueData = new ArrayList<>();
   }
 
   public String convertToText(int number) {
@@ -24,80 +23,56 @@ public class VenueHireSystem {
   }
 
   public void printVenues() {
-    this.bookings = bookings;
-    bookings = 0;
-
-    for (int i = 0; i < venueData.size(); i++) {
-      bookings++;
+    int bookings = allTheVenues.size(); // Update bookings count based on the size of allTheVenues
+    if (bookings == 0) {
+      MessageCli.NO_VENUES.printMessage();
+      return;
     }
+
     if (bookings == 1) {
       MessageCli.NUMBER_VENUES.printMessage("is", convertToText(bookings), "");
-      for (ArrayList<String> venue : venueData) {
-        String venueName = venue.get(0);
-        String venueCode = venue.get(1);
-        String capacity = venue.get(2);
-        String hireFee = venue.get(3);
-
-        MessageCli.VENUE_ENTRY.printMessage(venueName, venueCode, capacity, hireFee);
-      }
     } else if (bookings > 1 && bookings < 10) {
       MessageCli.NUMBER_VENUES.printMessage("are", convertToText(bookings), "s");
-      for (ArrayList<String> venue : venueData) {
-        String venueName = venue.get(0);
-        String venueCode = venue.get(1);
-        String capacity = venue.get(2);
-        String hireFee = venue.get(3);
-
-        MessageCli.VENUE_ENTRY.printMessage(venueName, venueCode, capacity, hireFee);
-      }
-    } else if (bookings >= 10) {
-      MessageCli.NUMBER_VENUES.printMessage("are", String.valueOf(bookings), "s");
-      for (ArrayList<String> venue : venueData) {
-        String venueName = venue.get(0);
-        String venueCode = venue.get(1);
-        String capacity = venue.get(2);
-        String hireFee = venue.get(3);
-
-        MessageCli.VENUE_ENTRY.printMessage(venueName, venueCode, capacity, hireFee);
-      }
-
     } else {
-      MessageCli.NO_VENUES.printMessage();
+      MessageCli.NUMBER_VENUES.printMessage("are", String.valueOf(bookings), "s");
+    }
+
+    for (venue venue : allTheVenues) {
+      MessageCli.VENUE_ENTRY.printMessage(venue.getVenuName(), venue.getVenueCode(), venue.getCapacity(),
+          venue.getHireFee());
     }
   }
 
   public void createVenue(
       String venueName, String venueCode, String capacityInput, String hireFeeInput) {
-    for (ArrayList<String> venue : venueData) {
-      if (venueCode.equals(venue.get(1))) {
-        MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venueCode, venue.get(0));
-        return; // Exit method if duplicate venue code found
+
+    venue venueBeingCreated = new venue(venueName, venueCode, capacityInput, hireFeeInput);
+
+    for (venue venue : allTheVenues) {
+      if (venue.getVenueCode().equals(venueCode)) {
+        MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venueCode, venue.getVenuName());
+        return;
       }
     }
 
     if (venueName.isEmpty()) {
       MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
+
     } else if (venueCode.isEmpty()) {
       System.out.println("Venue not created: venue code must not be empty.");
-      ;
+
     } else {
       try {
-        int capacity = Integer.parseInt(capacityInput);
-        int hireFee = Integer.parseInt(hireFeeInput);
+        int capacityInt = Integer.parseInt(capacityInput);
+        int hireFeeInt = Integer.parseInt(hireFeeInput);
 
-        if (capacity < 0) {
+        if (capacityInt < 0) {
           MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", " positive");
-
-        } else if (hireFee < 0) {
+        } else if (hireFeeInt < 0) {
           MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", " positive");
         } else {
           MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
-          ArrayList<String> venueDataItem = new ArrayList<>();
-          venueDataItem.add(venueName);
-          venueDataItem.add(venueCode);
-          venueDataItem.add(capacityInput);
-          venueDataItem.add(hireFeeInput);
-          venueData.add(venueDataItem);
+          allTheVenues.add(venueBeingCreated);
         }
       } catch (NumberFormatException e) {
         if (!capacityInput.matches("\\d+")) {
@@ -107,52 +82,18 @@ public class VenueHireSystem {
         }
       }
     }
+
   }
 
   public void setSystemDate(String dateInput) {
-    this.dateInput = dateInput;
-    MessageCli.DATE_SET.printMessage(dateInput);
 
   }
 
   public void printSystemDate() {
 
-    if (this.dateInput == null) {
-      MessageCli.CURRENT_DATE.printMessage("not set");
-    } else {
-      MessageCli.CURRENT_DATE.printMessage(this.dateInput);
-    }
   }
 
   public void makeBooking(String[] options) {
-    String gmail = options[2];
-    String date = options[1];
-    String bookingReference = BookingReferenceGenerator.generateBookingReference();
-    String attendees = options[3];
-    String venuName = null;
-
-    if (this.dateInput == null) {
-      MessageCli.BOOKING_NOT_MADE_DATE_NOT_SET.printMessage();
-      return;
-    }
-    if (venueData.isEmpty()) {
-      MessageCli.BOOKING_NOT_MADE_NO_VENUES.printMessage();
-      return;
-    }
-
-    for (ArrayList<String> venue : venueData) {
-      if (options[0].equals(venue.get(1))) {
-        venuName = venue.get(0);
-        break;
-      }
-    }
-    
-    if (venuName == null) {
-      MessageCli.BOOKING_NOT_MADE_VENUE_NOT_FOUND.printMessage(options[1]);
-      return;
-    } else {
-      MessageCli.MAKE_BOOKING_SUCCESSFUL.printMessage(bookingReference, venuName, date, attendees);
-    }
 
   }
 
--- src/main/java/nz/ac/auckland/se281/venue.java
+++ src/main/java/nz/ac/auckland/se281/venue.java
@@ -1,5 +1,41 @@
 package nz.ac.auckland.se281;
+import java.util.ArrayList;
+import nz.ac.auckland.se281.Types.CateringType;
+import nz.ac.auckland.se281.Types.FloralType;
+
 
 public class venue {
+  
+  
+  private String venuName;
+  private String venueCode;
+  private String capacity;
+  private String hireFee;
 
+  public venue(String venuName, String venueCode, String capacity, String hireFee) {
+    this.venuName = venuName;
+    this.venueCode = venueCode;
+    this.capacity = capacity;
+    this.hireFee = hireFee;
+   
+  }
+
+  public String getVenuName() {
+    return this.venuName;
+  }
+
+  public String getVenueCode() {
+    return this.venueCode;
+  }
+
+  public String getCapacity() {
+    return this.capacity;
+  }
+
+  public String getHireFee() {
+    return this.hireFee;
+  }
+
+
+  
 }
